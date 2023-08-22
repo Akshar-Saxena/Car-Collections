57df6a694ba2757df3985ed13f242eb2d9746e07
Akshar-Saxena <aksharsaxena2003@gmail.com>
2023-08-22 18:33:16 +0530

    changed url

--- layouts/info.js
+++ layouts/info.js
@@ -1,10 +1,82 @@
 const data = [
     {
         carName: "Mazda",
+        estd: "October 1931",
+        ctr: "Japan",
+        owner: "Toyota, The Master Trust Bank of Japan, Trust & Custody Services Bank",
+        value: "INR 514.141 billion",
         desc: "Mazda cars are known for their sleek design, engaging driving dynamics, and a balance between performance and efficiency. Here's a general description of Mazda cars. Mazda cars are a product of the Japanese automotive manufacturer Mazda Motor Corporation. Renowned for their distinctive \"Kodo\" design language, which emphasizes fluidity and an emotional connection with the driver, Mazda vehicles often stand out with their sculpted lines, bold front grilles, and attention to detail."
     },
     {
         carName: "Honda",
-        desc: "asfsfsfsfsdf"
+        estd: "September 1948",
+        ctr: "Japan",
+        owner: "Takahiro Hachigo",
+        value: "INR 4.63 trillion",
+        desc: "Honda Motors, officially known as Honda Motor Co., Ltd., is a renowned Japanese multinational corporation primarily known for its production of automobiles, motorcycles, and power equipment. Founded by Soichiro Honda and Takeo Fujisawa in 1948, Honda has established itself as one of the world's leading automotive and motorcycle manufacturers. Honda offers a wide range of products, including automobiles, motorcycles, power equipment (such as lawnmowers and generators), and even aviation products like jet engines."
+    },
+    {
+        carName: "Ford",
+        estd: "June 1903",
+        ctr: "United States",
+        owner: "William Clay Ford Jr.",
+        value: "INR 3.97 trillion",
+        desc: "Ford, officially known as Ford Motor Company, is an iconic American multinational corporation recognized for its historic role in shaping the global automotive industry. Founded by Henry Ford on June 16, 1903, Ford has played a significant role in making automobiles accessible to the masses and revolutionizing manufacturing processes. Ford is famously credited with introducing the concept of assembly line production to automobile manufacturing. Over the years, Ford has introduced various technological innovations to the automotive industry."
+    },
+    {
+        carName: "Toyota",
+        estd: "August 1937",
+        ctr: "Japan",
+        owner: "Koji Sato",
+        value: "INR 22.42 trillion",
+        desc: "Toyota Motor Corporation, commonly known as Toyota, is a renowned Japanese multinational automotive manufacturer with a global presence. Founded by Kiichiro Toyoda in 1937, Toyota has become one of the world's largest and most well-respected automobile companies, known for its innovation, reliability, and efficient manufacturing practices. Toyota is renowned for its dedication to producing vehicles that are known for their quality, durability, and reliability."
+    },
+    {
+        carName: "BMW",
+        estd: "March 1916",
+        ctr: "Germany",
+        owner: "Susanne Klatten, Stefan Quandt",
+        value: "INR 5.31 trillion",
+        desc: "Bayerische Motoren Werke AG, commonly known as BMW, is a renowned German automobile and motorcycle manufacturer founded in 1916. BMW has established itself as a symbol of luxury, performance, and engineering excellence, with a reputation for producing vehicles that offer a combination of driving pleasure, innovation, and premium quality.  BMW is often celebrated for its emphasis on delivering driving pleasure and exceptional performance."
+    },
+    {
+        carName: "Nissan",
+        estd: "December 1933",
+        ctr: "Japan",
+        owner: "Hiroto Saikawa",
+        value: "INR 1.43 trillion",
+        desc: "Nissan Motor Corporation is a prominent Japanese multinational automobile manufacturer that has been a significant player in the automotive industry since its establishment in 1933. Nissan is known for producing a diverse range of vehicles that cater to various market segments while emphasizing innovation, technology, and global presence. Nissan has a history of introducing innovative technologies to the automotive market."
+    },
+    {
+        carName: "Chevrolet",
+        estd: "November 1911",
+        ctr: "United States",
+        owner: "Mary Barra",
+        value: "INR 3.77 trillion",
+        desc: "Chevrolet, often referred to as Chevy, is an iconic American automotive brand that has been a part of the automotive landscape for over a century. Founded in 1911 by Louis Chevrolet and William C. Durant, Chevrolet is known for producing a wide range of vehicles, from cars and trucks to SUVs, with a strong emphasis on innovation, performance, and affordability.  Chevrolet has a rich history that dates back to its founding in the early 20th century."
+    },
+    {
+        carName: "Audi",
+        estd: "July 1909",
+        ctr: "Germany",
+        owner: "Gernot Döllner",
+        value: "INR 6.88 trillion",
+        desc: "Audi is a renowned German luxury automobile manufacturer that has established itself as a symbol of sophistication, innovation, and engineering excellence. Founded in 1909, Audi has a storied history in the automotive industry and is known for producing a wide range of vehicles that combine performance, technology, and luxury. Audi is often associated with its distinctive design language, characterized by clean lines, modern aesthetics, and attention to detail."
+    },
+    {
+        carName: "Dodge",
+        estd: "November 1914",
+        ctr: "United States",
+        owner: "Tim Kuniskis",
+        value: "INR 1.02 trillion",
+        desc: "Dodge, an American automobile brand, is known for producing a variety of cars, some of which have become iconic in the automotive world. As of my last knowledge update in September 2021, here are some key Dodge car models, The Dodge Charger is a four-door sedan that blends performance with practicality. It's known for its powerful engine options, including V6 and V8 choices, as well as its iconic design. The Charger has a strong following among enthusiasts."
+    },
+    {
+        carName: "Porsche",
+        estd: "April 1931",
+        ctr: "Germany",
+        owner: "Oliver Blume",
+        value: "INR 4.19 trillion",
+        desc: "Porsche is a renowned German automobile manufacturer that specializes in high-performance sports cars, luxury vehicles, and SUVs. Established in 1931 by Ferdinand Porsche, the company has a rich history and a reputation for engineering excellence and innovation. As of my last knowledge update in September 2021. Porsche vehicles are known for their exceptional performance and precision engineering. Porsche cars are instantly recognizable by their distinctive design language."
     }
 ]
